b932d2b3becf96bd9a546750a646e9722c1c6d15
alemalagni <alemalagni@gmail.com>
2025-03-07 15:05:32 +0100

    feat: add script section

--- snack-01/js/app.js
+++ snack-01/js/app.js
@@ -1,7 +1,17 @@
+// Dichiarazione
 const bici = [
     ["Bici 01", 7.1]
     ["Bici 02", 7.8]
     ["Bici 03", 7.3]
     ["Bici 04", 6.9]
     ["Bici 05", 7.4]
-]
+];
+
+// Script
+let minorPeso = 0;
+for ( let i = 0; i < bici.length; i++) {
+    if ( bici[i][1] < minorPeso ) {
+        
+        minorPeso = bici[i][1];
+    };
+};
