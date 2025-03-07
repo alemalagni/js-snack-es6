954119b85f4c6dfa045d5ec0a7c0133aa84d45b5
alemalagni <alemalagni@gmail.com>
2025-03-07 15:26:28 +0100

    feat: add array

---
+++ snack-02/js/app.js
@@ -0,0 +1,68 @@
+const squadra = [
+    // 1
+    {
+        'nome' : "Giocatore 1",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 2
+    {
+        'nome' : "Giocatore 2",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 3
+    {
+        'nome' : "Giocatore 3",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 4
+    {
+        'nome' : "Giocatore 4",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 5
+    {
+        'nome' : "Giocatore 5",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 6
+    {
+        'nome' : "Giocatore 6",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 7
+    {
+        'nome' : "Giocatore 7",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 8
+    {
+        'nome' : "Giocatore 8",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 9
+    {
+        'nome' : "Giocatore 9",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 10
+    {
+        'nome' : "Giocatore 10",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+    // 11
+    {
+        'nome' : "Giocatore 11",
+        'puntiFatti' : parseInt(Math.random() * 20),
+        'falliSubiti' : parseInt(Math.random() * 20),
+    },
+]
