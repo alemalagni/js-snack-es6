f315d63f3f94e51d891c3b133759c5cf61d0b35f
alemalagni <alemalagni@gmail.com>
2025-03-07 15:38:54 +0100

    fix: script

--- snack-02/js/app.js
+++ snack-02/js/app.js
@@ -6,11 +6,71 @@ const squadra = [
         'puntiFatti' : 0,
         'falliSubiti' : 0
     },
+    // 2
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 3
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 4
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 5
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 6
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 7
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 8
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 9
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 10
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    },
+    // 11
+    {
+        'nome' : "",
+        'puntiFatti' : 0,
+        'falliSubiti' : 0
+    }
 ];
 
 // Script
 for ( let i = 0; i < 11; i++) {
-    squadra[i].nome = "Giocatore " + i;
-    squadra[i].puntiFatti = parseInt(Math.random() * 20),
-    squadra[i].falliSubiti = parseInt(Math.random() * 20)
+    squadra[i].nome = `Giocatore ${i}`;
+    squadra[i].puntiFatti = parseInt(Math.random() * 20);
+    squadra[i].falliSubiti = parseInt(Math.random() * 20);
 };
